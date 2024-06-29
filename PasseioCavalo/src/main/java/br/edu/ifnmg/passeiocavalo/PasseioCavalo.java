340b5523a437ed6e86882a8749e3104b23f66fe4
Gustavo <gustavo.l.mav@gmail.com>
2024-06-29 11:49:04 -0300

    inicializar

---
+++ PasseioCavalo/src/main/java/br/edu/ifnmg/passeiocavalo/PasseioCavalo.java
@@ -0,0 +1,12 @@
+package br.edu.ifnmg.passeiocavalo;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class PasseioCavalo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
