187f14b517c97287411f4183e557240329d8717a
Gustavo <gustavo.l.mav@gmail.com>
2024-06-30 16:26:14 -0300

    movimentação Cavalo

--- PasseioCavalo/src/main/java/br/edu/ifnmg/passeiocavalo/Main.java
+++ PasseioCavalo/src/main/java/br/edu/ifnmg/passeiocavalo/Main.java
@@ -1,20 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package br.edu.ifnmg.passeiocavalo;
 
 /**
  *
- * @author agatha
+ * @author Gustavo
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        PasseioCavalo cavalo = new PasseioCavalo(0,7);
+
+        PasseioCavalo.Movimento(cavalo);
     }
     
 }
--- PasseioCavalo/src/main/java/br/edu/ifnmg/passeiocavalo/PasseioCavalo.java
+++ PasseioCavalo/src/main/java/br/edu/ifnmg/passeiocavalo/PasseioCavalo.java
@@ -6,7 +6,55 @@ package br.edu.ifnmg.passeiocavalo;
  */
 public class PasseioCavalo {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    int[][] tabuleiro = new int[8][8];
+    //Por não passar nenhum valor, em Java inicializar int como 0;
+    
+    int[] horizontal = {2,1,-1,-2,-2,-1,1,2};
+    int[] vertical = {-1,-2,-2,-1,1,2,2,1};
+    
+    int currentRow, currentColumn, moveNumber;
+    int contador; //Variavel para contar quantas casas o cavalo passou
+    
+    
+    PasseioCavalo(int x, int y){ //Construtor x=horizontal y=vertical
+        this.currentColumn = y;
+        this.currentRow = x;
+        this.tabuleiro[x][y] = 1;
+        //O valor inicial do tabuleiro já começa marcado.
+        
+        this.contador = 1;
     }
+    
+    public static void Movimento(PasseioCavalo cavalo){
+        
+        for(cavalo.moveNumber = 0; cavalo.moveNumber < 8; cavalo.moveNumber++){
+            //Verifica se está dentro do tabuleiro
+            if(cavalo.currentColumn+cavalo.vertical[cavalo.moveNumber] > -1
+               && cavalo.currentRow+cavalo.horizontal[cavalo.moveNumber] > -1
+               && cavalo.currentColumn+cavalo.vertical[cavalo.moveNumber] < 8
+               && cavalo.currentRow+cavalo.horizontal[cavalo.moveNumber] < 8)
+            {
+                //vertifica se já passou pela casa
+                if(cavalo.tabuleiro
+/*Linha*/         [cavalo.currentRow+cavalo.horizontal[cavalo.moveNumber]]
+/*Coluna*/        [cavalo.currentColumn+cavalo.vertical[cavalo.moveNumber]]!=1)
+                {
+                    
+                    System.out.println("["+cavalo.currentRow+"]["+cavalo.currentColumn+"]");
+                    cavalo.currentColumn += cavalo.vertical[cavalo.moveNumber];
+                    cavalo.currentRow += cavalo.horizontal[cavalo.moveNumber];
+                    cavalo.tabuleiro[cavalo.currentRow][cavalo.currentColumn] = 1;
+                    cavalo.contador+=1;
+                    cavalo.moveNumber=0;
+                    
+                }
+                
+            }
+        }
+        System.out.println("["+cavalo.currentRow+"]["+cavalo.currentColumn+"]");
+        System.out.println(cavalo.contador);
+    }
+    
+    
+    
 }
